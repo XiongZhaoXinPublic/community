a4df27f04155190e893f312c0f307876256d7b69
xiongzhaoxin <1210094565@qq.com>
2019-07-09 21:48:06 +0800

    add check cookies

--- src/main/java/life/majiang/community/Controller/AuthorizeControllor.java
+++ src/main/java/life/majiang/community/Controller/AuthorizeControllor.java
@@ -4,11 +4,18 @@ package life.majiang.community.Controller;
 import life.majiang.community.Provide.GithubProvide;
 import life.majiang.community.dto.AccesstokenDTO;
 import life.majiang.community.dto.GithubUser;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 
 @Controller
@@ -25,10 +32,14 @@ public class AuthorizeControllor {
     @Value("${github.redirect.uri}")
     private String RedirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccesstokenDTO accesstokenDTO = new AccesstokenDTO();
         accesstokenDTO.setClient_id(Client);
         accesstokenDTO.setClient_secret(ClientSecret);
@@ -36,11 +47,24 @@ public class AuthorizeControllor {
         accesstokenDTO.setRedirect_uri(RedirectUri);
         accesstokenDTO.setState(state);
         String acessToken = githubProvide.getAcessToken(accesstokenDTO);
-        GithubUser user = githubProvide.getUser(acessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser gitUser = githubProvide.getUser(acessToken);
+        if (gitUser!=null){
+            //登入成功，写cookie和session
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(gitUser.getName());
+            user.setAccountId(String.valueOf(gitUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModefied(user.getGmtCreate());
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
+
+            request.getSession().setAttribute("user",gitUser);
+            return "redirect:/";
+        }else {
+            //登入失败，重新登入
+            return "redirect:/";
+        }
     }
-
-
-
 }
--- src/main/java/life/majiang/community/Controller/IndexContrallor.java
+++ src/main/java/life/majiang/community/Controller/IndexContrallor.java
@@ -1,11 +1,32 @@
 package life.majiang.community.Controller;
 
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexContrallor {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){return  "index";}
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies) {
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+
+                break;
+            }
+
+        }
+        return  "index";
+    }
 }
